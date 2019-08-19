b0ee2f99ee81850ee2900dbdc32a86f3ec82ab32
Mathieu Depetris <mathieu.depetris@ird.fr>
2019-08-19 13:50:24 +0200

    Doc updated

--- R/furdeb.R
+++ R/furdeb.R
@@ -3,22 +3,28 @@
 #'\itemize{
 #' \item{\code{\link{configuration_file}}}
 #' \item{\code{\link{ocean_code_to_name}}}
+#' \item{\code{\link{utils-pipe}}}
 #' }
 #'@section Databases manipulations and connections:
 #'\itemize{
 #' \item{\code{\link{access_dbconnection}}}
-#' \item{\code{\link{configuration_file}}}
 #' \item{\code{\link{db_connection}}}
 #' \item{\code{\link{mdb2Sqlite3}}}
 #' \item{\code{\link{sql_inset}}}
 #' }
 #' @section Spatial manipulations and design:
 #'\itemize{
+#' \item{\code{\link{avdth_position_conversion_dec}}}
 #' \item{\code{\link{fao_area_overlay}}}
 #' \item{\code{\link{fao_area_overlay_unassociated}}}
 #' \item{\code{\link{lat_long_to_csquare}}}
 #' \item{\code{\link{cwp_to_center}}}
 #' \item{\code{\link{cwp_to_lat_lon}}}
+#' }
+#' @section Referentials:
+#'\itemize{
+#' \item{\code{\link{fishing_mode_code_to_name}}}
+#' \item{\code{\link{ocean_code_to_name}}}
 #' }
 #' @docType package
 #' @name furdeb
