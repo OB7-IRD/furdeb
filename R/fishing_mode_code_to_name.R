b0925534ba132be2c83a7a405c578be9b00548af
depetris <mathieu.depetris@ird.fr>
2019-08-23 16:23:02 +0200

    Updated and added new function

--- R/fishing_mode_code_to_name.R
+++ R/fishing_mode_code_to_name.R
@@ -3,6 +3,7 @@
 #' @name fishing_mode_to_name
 #' @author Mathieu Depetris, \email{mathieu.depetris@@ird.fr}
 #' @param fishing_mode_code Fishing mode code(s) code(s) in numerical values.
+#' @param detail If you want to display all the fishing modes name or a shortcut with "all fishing modes". By default TRUE.
 #' @references \url{https://github.com/OB7-IRD/furdeb}
 #' @return A character vector in relation with the fishing mode(s) code(s) provided.
 #' @export
@@ -15,37 +16,49 @@ fishing_mode_to_name <- function (fishing_mode_code) {
          "\n",
          "Please correct it before running the function.")
   }
-
   # Function ----
   if (length(fishing_mode_code) == 1) {
     if (fishing_mode_code == 1) {
       fishing_mode_name <- "floating object"
+      fishing_mode_code_chr <- "FOB"
     } else {
       if (fishing_mode_code == 2) {
         fishing_mode_name <- "free school"
+        fishing_mode_code_chr <- "FSC"
       } else {
         if (fishing_mode_code == 3) {
           fishing_mode_name <- "undetermined school"
+          fishing_mode_code_chr <- "UND"
         }
       }
     }
   } else {
     fishing_mode_name <- NULL
+    fishing_mode_code_chr <- NULL
     for (i in fishing_mode_code) {
       if (i == 1) {
         fishing_mode_name <- ifelse(is.null(fishing_mode_name),
                                     "floating object, ",
                                     paste0(fishing_mode_name, "floating object, "))
+        fishing_mode_code_chr <- ifelse(is.null(fishing_mode_code_chr),
+                                        "FOB, ",
+                                        paste0(fishing_mode_code_chr, "FOB, "))
       } else {
         if (i == 2) {
           fishing_mode_name <- ifelse(is.null(fishing_mode_name),
                                       "free school, ",
                                       paste0(fishing_mode_name, "free school, "))
+          fishing_mode_code_chr <- ifelse(is.null(fishing_mode_code_chr),
+                                          "FSC, ",
+                                          paste0(fishing_mode_code_chr, "FSC, "))
         } else {
           if (i == 3) {
             fishing_mode_name <- ifelse(is.null(fishing_mode_name),
                                         "undetermined school, ",
                                         paste0(fishing_mode_name, "undetermined school, "))
+            fishing_mode_code_chr <- ifelse(is.null(fishing_mode_code_chr),
+                                            "UND, ",
+                                            paste0(fishing_mode_code_chr, "UND, "))
           }
         }
       }
@@ -57,7 +70,15 @@ fishing_mode_to_name <- function (fishing_mode_code) {
                                         collapse = ", "),
                                   " and ",
                                   dplyr::last(tmp))
+      tmp <- stringr::str_split(string = fishing_mode_code_chr, pattern = ", ", simplify = TRUE)
+      tmp <- tmp[tmp != ""]
+      fishing_mode_code_chr <- paste0(paste(tmp[tmp != dplyr::last(tmp)],
+                                            collapse = ", "),
+                                      " and ",
+                                      dplyr::last(tmp))
     }
   }
-  return(fishing_mode_name)
+  fishing_mode <- c("fishing_mode_name" = fishing_mode_name,
+                    "fishing_mode_code_chr" = fishing_mode_code_chr)
+  return(fishing_mode)
 }
--- R/ocean_code_to_name.R
+++ R/ocean_code_to_name.R
@@ -15,26 +15,31 @@ ocean_code_to_name <- function (ocean_code) {
          "\n",
          "Please correct it before running the function.")
   }
-
   # Function ----
   if (length(ocean_code) == 1) {
     if (ocean_code == 1) {
       ocean_name <- "Atlantic Ocean"
+      ocean_code_chr <- "AO"
     } else {
       if (ocean_code == 2) {
         ocean_name <- "Indian Ocean"
+        ocean_code_chr <- "IO"
       } else {
         if (ocean_code == 3) {
           ocean_name <- "West Pacific Ocean"
+          ocean_code_chr <- "WPO"
         } else {
           if (ocean_code == 4) {
             ocean_name <- "East Pacific Ocean"
+            ocean_code_chr <- "EPO"
           } else {
             if (ocean_code == 5) {
               ocean_name <- "Pacific Ocean"
+              ocean_code_chr <- "PO"
             } else {
               if (ocean_code == 6) {
                 ocean_name <- "Undetermined"
+                ocean_code_chr <- "UND"
               }
             }
           }
@@ -43,36 +48,55 @@ ocean_code_to_name <- function (ocean_code) {
     }
   } else {
     ocean_name <- NULL
+    ocean_code_chr <- NULL
     for (i in ocean_code) {
       if (i == 1) {
         ocean_name <- ifelse(is.null(ocean_name),
                              "Atlantic, ",
                              paste0(ocean_name, "Atlantic, "))
+        ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                 "AO, ",
+                                 paste0(ocean_code_chr, "AO, "))
       } else {
         if (i == 2) {
           ocean_name <- ifelse(is.null(ocean_name),
                                "Indian, ",
                                paste0(ocean_name, "Indian, "))
+          ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                   "IO, ",
+                                   paste0(ocean_code_chr, "IO, "))
         } else {
           if (i == 3) {
             ocean_name <- ifelse(is.null(ocean_name),
                                  "West Pacific, ",
                                  paste0(ocean_name, "West Pacific, "))
+            ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                     "WPO, ",
+                                     paste0(ocean_code_chr, "WPO, "))
           } else {
             if (i == 4) {
               ocean_name <- ifelse(is.null(ocean_name),
                                    "East Pacific, ",
                                    paste0(ocean_name, "East Pacific, "))
+              ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                       "EPO, ",
+                                       paste0(ocean_code_chr, "EPO, "))
             } else {
               if (i == 5) {
                 ocean_name <- ifelse(is.null(ocean_name),
                                      "Pacific, ",
                                      paste0(ocean_name, "Pacific, "))
+                ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                         "PO, ",
+                                         paste0(ocean_code_chr, "PO, "))
               } else {
                 if (i == 6) {
                   ocean_name <- ifelse(is.null(ocean_name),
                                        "Undetermined, ",
                                        paste0(ocean_name, "Undetermined, "))
+                  ocean_code_chr <- ifelse(is.null(ocean_code_chr),
+                                           "UND, ",
+                                           paste0(ocean_code_chr, "UND, "))
                 }
               }
             }
@@ -88,7 +112,15 @@ ocean_code_to_name <- function (ocean_code) {
                            " and ",
                            dplyr::last(tmp),
                            " Oceans")
+      tmp <- stringr::str_split(string = ocean_code_chr, pattern = ", ", simplify = TRUE)
+      tmp <- tmp[tmp != ""]
+      ocean_code_chr <- paste0(paste(tmp[tmp != dplyr::last(tmp)],
+                                     collapse = ", "),
+                               " and ",
+                               dplyr::last(tmp))
     }
   }
-  return(ocean_name)
+  ocean <- c("ocean_name" = ocean_name,
+             "ocean_code_chr" = ocean_code_chr)
+  return(ocean)
 }
