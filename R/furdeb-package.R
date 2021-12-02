38bc74db395aef822a6b458aac26f70ce4b76044
Mathieu DEPETRIS <35579752+MathieuDepetris@users.noreply.github.com>
2021-12-02 14:45:46 +0100

    Update logo

--- R/furdeb-package.R
+++ R/furdeb-package.R
@@ -1,19 +1,19 @@
-#'@title furdeb package
-#'@section Databases manipulations:
-#'\itemize{
+#' @title furdeb package
+#' @section Databases manipulations:
+#' \itemize{
 #' \item{\code{\link{access_dbconnection}}}
 #' \item{\code{\link{postgresql_dbconnection}}}
 #' }
 #' @section Spatial design:
-#'\itemize{
+#' \itemize{
 #' \item{\code{\link{avdth_position_conversion_dec}}}
 #' \item{\code{\link{fao_reference_grid_update}}}
 #' \item{\code{\link{lat_lon_cwp_manipulation}}}
 #' \item{\code{\link{lat_long_to_csquare}}}
 #' \item{\code{\link{marine_area_overlay}}}
 #' }
-#'@section Setup help:
-#'\itemize{
+#' @section Setup help:
+#' \itemize{
 #' \item{\code{\link{configuration_file}}}
 #' }
 #' @keywords internal
